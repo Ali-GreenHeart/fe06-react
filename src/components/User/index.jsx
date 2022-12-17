54d621a9161a1f44c7fcd91ac0e54380a88c2793
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2022-12-17 12:46:45 +0400

    change alt of user image

--- src/components/User/index.jsx
+++ src/components/User/index.jsx
@@ -4,7 +4,7 @@ import styles from './index.module.css'
 const User = ({ basliq, description, imgSrc }) => {
     return (
         <div className={styles.card}>
-            <img src={imgSrc} alt='ali developer' />
+            <img src={imgSrc} alt={`${basliq} ${description}`} />
             <h3>{basliq}</h3>
             <p>{description}</p>
         </div>
