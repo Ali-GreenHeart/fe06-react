7416c27495d7d430f6af742b1e59db86d4b4cfd8
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2022-12-17 13:00:38 +0400

    add routes and home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import Counter from "components/Counter";
+import HomePage from "pages/Home";
+import { Route, Routes } from "react-router-dom";
 import Users from "./components/Users";
 
 function App() {
   return (
     <>
-      <Users />
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/counter" element={<Counter />} />
+        <Route path="/users" element={<Users />} />
+      </Routes>
     </>
   );
 }
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,8 @@
+const HomePage = () => {
+    return (
+        <h1>Welcome home!</h1>
+    )
+}
+
+
+export default HomePage;
