70b62be49300dd4df2c66c6dd1645a0dfde5a31f
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2022-12-17 13:49:06 +0400

    move routing to /route folder

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import Counter from "components/Counter";
-import AboutPage from "pages/About";
-import HomePage from "pages/Home";
-import { Route, Routes } from "react-router-dom";
-import Users from "./components/Users";
-import Service from "./pages/Service";
+import WebsiteRouting from "route";
 
-function App() {
+
+
+const App = () => {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/counter" element={<Counter />} />
-        <Route path="/users" element={<Users />} />
-        <Route path="/about" element={<AboutPage />}/>
-        <Route path="/service" element={<Service />}/>
-      </Routes>
+      <WebsiteRouting />
     </>
   );
 }
---
+++ src/route/index.jsx
@@ -0,0 +1,22 @@
+import Counter from "components/Counter";
+import AboutPage from "pages/About";
+import HomePage from "pages/Home";
+import { Route, Routes } from "react-router-dom";
+import Users from "components/Users";
+import Service from "pages/Service";
+
+const WebsiteRouting = () => {
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/counter" element={<Counter />} />
+                <Route path="/users" element={<Users />} />
+                <Route path="/about" element={<AboutPage />} />
+                <Route path="/service" element={<Service />} />
+            </Routes>
+        </>
+    )
+}
+
+export default WebsiteRouting;
